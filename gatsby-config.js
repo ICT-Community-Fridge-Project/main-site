3df888c55746c3928a0d4c1e80e55623eb456aae
petermiles <loaatao@gmail.com>
2020-12-03 18:51:49 -0600

    fix: change trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,7 @@ module.exports = {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-32778170-9",
+        trackingId: "G-B0858R2NJE",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
